ba1b5d53ff9c94d0db5a2822e3009f1895761ddc
lucas bamber <lucasbamber1@outlook.com>
2017-11-06 13:56:19 +0000

    ex 1

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work!"
+      @response = "Great!"
+    elsif @question.include? "?"
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don’t care, get dressed and go to work!"
+    end
+  end
+end
